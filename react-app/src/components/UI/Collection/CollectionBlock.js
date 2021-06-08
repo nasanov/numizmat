5c71602741d8211d81d9f7eb29ca65ee2d272775
nasanov <asanoff.n@gmail.com>
2021-06-08 17:49:56 -0500

    delete collections

--- react-app/src/components/UI/Collection/CollectionBlock.js
+++ react-app/src/components/UI/Collection/CollectionBlock.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './CollectionBlock.css';
+import { useDispatch } from 'react-redux';
+
+import EditCollectionModal from './EditCollectionModal/EditCollectionModal';
+import { removeCollection } from '../../../store/collections';
+
 export default function CollectionBlock({ collection }) {
-	// console.log(collection);
+	const dispatch = useDispatch();
+
+	const deleteCollectionHandler = () => {
+		dispatch(removeCollection(collection.id));
+	};
+
 	return (
 		<div className="collection__container">
 			<NavLink to={`/collections/${collection.id}`}>
 				<img src="https://i.stack.imgur.com/y9DpT.jpg" width="300" className="collection__image"></img>
+			</NavLink>
+			<NavLink to={`/collections/${collection.id}`}>
 				<div className="collection__title">
 					<span className="collection__title--value">
 						<span className="collection__title--name">Name: </span>
@@ -18,6 +30,18 @@ export default function CollectionBlock({ collection }) {
 					</span>
 				</div>
 			</NavLink>
+			<div className="collection__btns">
+				{collection.name === 'All Coins' || collection.name === 'Wishlist' ? (
+					<></>
+				) : (
+					<>
+						<EditCollectionModal collection={collection} />
+						<button className="coin__add-to-collection--btn" onClick={deleteCollectionHandler}>
+							Delete collection
+						</button>
+					</>
+				)}
+			</div>
 		</div>
 	);
 }
